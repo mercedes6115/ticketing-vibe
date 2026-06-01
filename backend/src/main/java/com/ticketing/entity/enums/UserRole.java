e9fbc59d6e48690818a88b8ebf2b2873764ece45
bonyoung.bae <bp6115@naver.com>
2026-06-01 17:36:48 +0900

    Deploy portfolio site

--- backend/src/main/java/com/ticketing/entity/enums/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.ticketing.entity.enums;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
--- backend/src/main/java/com/ticketing/exception/ForbiddenException.java
+++
@@ -1,8 +0,0 @@
-package com.ticketing.exception;
-
-public class ForbiddenException extends RuntimeException {
-
-    public ForbiddenException(String message) {
-        super(message);
-    }
-}
